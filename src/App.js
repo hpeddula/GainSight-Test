a66526348e196a2551e53fe0dec360d63e672817
Harsha <hpeddula@outlook.in>
2022-11-29 08:39:13 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./styles.css";
-import {useEffect} from 'react'
+import { useEffect, useState } from "react";
+import moment from 'moment'
 
 export default function App() {
-  
+  //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
+  const [commits, setCommits] = useState([]);
+  const fetchCommits = async () => {
+    const fetchedCommits = await fetch(
+      "https://api.github.com/repos/hpeddula/GainSight-Test/commits",
+      {
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer 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`,
+        },
+      }
+    );
+    const finalData = await fetchedCommits.json();
+    setCommits(finalData);
+  };
+  useEffect(() => {
+    fetchCommits();
+  }, []);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Git Hub Commits</h2>
+      <ul>
+        {commits?.map(({commit:{author:{name,date},message}},index) => (
+          <li key={index}>{name} : {message} {moment(date).format('MMM YY, h:mm A')}</li> 
+        ))}
+      </ul>
     </div>
   );
 }
